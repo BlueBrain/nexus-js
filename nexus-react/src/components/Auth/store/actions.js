ffe000379620175e34e123bf546c6469219c654e
Dupont Michaël <michael.dupont@epfl.ch>
2018-07-16 13:05:26 +0200

    Support base64url encoding from JWT in Auth.

--- nexus-react/src/components/Auth/store/actions.js
+++ nexus-react/src/components/Auth/store/actions.js
@@ -3,12 +3,21 @@ import * as types from "./types";
 
 const getLoginObjectFromURL = URL => {
   try {
+    // Token in JWT format
     const token = removeTokenFromUrl(URL);
     if (!token) { return {} }
+
+    // JWT structure is "base64url(header) . base64url(payload) . base64url(signature)"
     const [, payload, ] = token.split('.');
+
+    // Convert payload from base64url to plain base64.
+    // Only 2 characters differ.
+    const payloadBase64 = payload.replace(/-/g, '+').replace(/_/g, '/');
+
+
     // Go through URL decoding to properly parse UTF-8 data.
     // @see https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_Unicode_Problem
-    const { name, exp } = JSON.parse(decodeURIComponent(escape(atob(payload))));
+    const { name, exp } = JSON.parse(decodeURIComponent(escape(atob(payloadBase64))));
     return { token, tokenExpiration: exp, tokenOwner: name }
   } catch (e) {
     console.error(e);

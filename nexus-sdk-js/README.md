fd8abe8e3e2a5058894c6cbf586a4151bf8abaaf
Julien Machon <julienmachon@gmail.com>
2018-11-28 10:34:35 +0100

    v0.0.1 is ready 💥 (#23)
    * exposing types and classes
    
    * add listPorjects
    
    * update licence
    
    * update version
    
    * rename method to getOrg
    
    * some get started doc
    
    * add licence to readme
    
    * rewording + remove useless doc

--- nexus-sdk-js/README.md
+++ nexus-sdk-js/README.md
@@ -12,8 +12,25 @@
 ```typescript
 import Nexus from 'nexus-sdk';
 
+// Create a Nexus instance
 const nexus: Nexus = new Nexus({
   environment: 'http://api.url',
-  token: 'my_bearer_token',
+  token: 'my_bearer_token', // optional
 });
+
+// List all organisations
+const orgs: Organization[] = await nexus.listOrganizations();
+
+// Get a specific organisation
+const myOrg: Organization = await nexus.getOrganization('my-org');
+
+// List all the projects that belong to an organisation
+const projects: Project[] = await myOrg.listProjects();
+
+// Get a specific project
+const myProject: Project = await myOrg.getProject('my-project');
 ```
+
+# License
+
+- [Apache License, Version 2.0](https://www.apache.org/licenses/LICENSE-2.0)
--- nexus-sdk-js/src/Organization.ts
+++ nexus-sdk-js/src/Organization.ts
@@ -42,10 +42,45 @@ export default class Organization {
     this.uuid = organizationResponse._uuid;
   }
 
+  // TODO: refactor -> blocked by https://github.com/BlueBrain/nexus/issues/112
   async listProjects(): Promise<Project[]> {
     try {
-      const projects = await httpGet(`/projects/${this.name}`);
-      return projects.map((project: ProjectResponse) => new Project(project));
+      const listOrgsResponse: ListOrgsResponse = await httpGet('/projects');
+      if (listOrgsResponse.code || !listOrgsResponse._results) {
+        return [];
+      }
+      // Get list of unique orgs names
+      const filteredOrgNames: {
+        orgName: string;
+        projectName: string;
+      }[] = listOrgsResponse._results
+        .map(org => {
+          const split = org._id.split('/');
+          const [orgName, projectName] = split.slice(
+            split.length - 2,
+            split.length,
+          );
+          return { orgName, projectName };
+        })
+        .filter(({ orgName }) => orgName === this.label);
+
+      return Promise.all(
+        filteredOrgNames.map(
+          async ({ projectName }) => await this.getProject(projectName),
+        ),
+      );
+    } catch (e) {
+      return e;
+    }
+  }
+
+  async getProject(projectName: string): Promise<Project> {
+    try {
+      const projectResponse: ProjectResponse = await httpGet(
+        `/projects/${this.label}/${projectName}`,
+      );
+      const project = new Project(projectResponse);
+      return project;
     } catch (e) {
       return e;
     }

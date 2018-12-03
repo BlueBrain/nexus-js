bccef9e4c6c935318213a3616d26bb25c09c2ced
Julien Machon <julienmachon@gmail.com>
2018-12-03 17:25:09 +0100

    List resources (#25)
    * npmignore and keyworks
    
    * add resources list

--- nexus-sdk-js/src/__tests__/Project.spec.ts
+++ nexus-sdk-js/src/__tests__/Project.spec.ts
@@ -2,41 +2,44 @@ import Project, { ProjectResponse } from '../Project';
 
 /* tslint:disable */
 const mockResponse: ProjectResponse = {
-  "@context": "https://bluebrain.github.io/nexus/contexts/project",
-  "@id": "https://nexus.example.com/v1/projects/myorg/myproject",
-  "@type": "nxv:Project",
-  "name": "example project creation",
-  "base": "https://nexus.example.com/v1/myorg/myproject/",
-  "prefixMappings": [
+  '@context': 'https://bluebrain.github.io/nexus/contexts/project',
+  '@id': 'https://nexus.example.com/v1/projects/myorg/myproject',
+  '@type': 'nxv:Project',
+  name: 'example project creation',
+  label: 'example',
+  base: 'https://nexus.example.com/v1/myorg/myproject/',
+  prefixMappings: [
     {
-      "prefix": "person",
-      "namespace": "http://example.com/some/person"
+      prefix: 'person',
+      namespace: 'http://example.com/some/person',
     },
     {
-      "prefix": "schemas",
-      "namespace": "https://bluebrain.github.io/nexus/schemas/"
+      prefix: 'schemas',
+      namespace: 'https://bluebrain.github.io/nexus/schemas/',
     },
     {
-      "prefix": "ex",
-      "namespace": "http://example.com/"
-    }
+      prefix: 'ex',
+      namespace: 'http://example.com/',
+    },
   ],
-  "_label": "myproject",
-  "_uuid": "968ad034-268a-4b07-aedd-219e3b2d8940",
-  "_self": "https://nexus.example.com/v1/projects/myorg/myproject",
-  "_constrainedBy": "nxs:project",
-  "_createdAt": "2018-09-18T09:58:00.801Z",
-  "_createdBy": "https://nexus.example.com/v1/realms/myrealm/users/f:ad46ddd6-134e-44d6-ab70-bdf00f48dfce:someone",
-  "_updatedAt": "2018-09-18T10:30:00.801Z",
-  "_updatedBy": "https://nexus.example.com/v1/realms/myrealm/users/f:ad46ddd6-134e-44d6-ab70-bdf00f48dfce:someone",
-  "_rev": 4,
-  "_deprecated": true
+  _label: 'myproject',
+  _uuid: '968ad034-268a-4b07-aedd-219e3b2d8940',
+  _self: 'https://nexus.example.com/v1/projects/myorg/myproject',
+  _constrainedBy: 'nxs:project',
+  _createdAt: '2018-09-18T09:58:00.801Z',
+  _createdBy:
+    'https://nexus.example.com/v1/realms/myrealm/users/f:ad46ddd6-134e-44d6-ab70-bdf00f48dfce:someone',
+  _updatedAt: '2018-09-18T10:30:00.801Z',
+  _updatedBy:
+    'https://nexus.example.com/v1/realms/myrealm/users/f:ad46ddd6-134e-44d6-ab70-bdf00f48dfce:someone',
+  _rev: 4,
+  _deprecated: true,
 };
 /* tslint:enable */
 
 describe('Project class', () => {
   it('Should create an Project instance', () => {
-    const p = new Project(mockResponse);
+    const p = new Project('my-org', mockResponse);
     expect(p.id).toEqual(mockResponse['@id']);
     expect(p.name).toEqual(mockResponse.name);
     expect(p.base).toEqual(mockResponse.base);

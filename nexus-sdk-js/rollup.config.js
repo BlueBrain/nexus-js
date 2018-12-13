e1ecb2b5c96eae0b68075a59a612e722399bd3c8
Julien Machon <julienmachon@gmail.com>
2018-12-13 11:41:52 +0100

    ignore test files in compiled outputs

--- nexus-sdk-js/rollup.config.js
+++ nexus-sdk-js/rollup.config.js
@@ -3,8 +3,6 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
-import pkg from './package.json';
-
 const libName = 'nexus-sdk';
 const UMDName = 'Nexus';
 

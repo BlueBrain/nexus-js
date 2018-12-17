54d70ef3d8ed915f6b7c84fb36ce2052e15a050f
Julien Machon <julienmachon@gmail.com>
2018-12-17 15:51:40 +0100

    quick documentation

--- nexus-sdk-js/README.md
+++ nexus-sdk-js/README.md
@@ -65,6 +65,12 @@ const filteredByType: PaginatedList<Resource> = await myView.filterByTypes(["som
 // ESView Query Convenience Method
 // Filter a project by resources matching a schema ("_constrainedBy")
 const filteredByConstrainedBy: PaginatedList<Resource> = await myView.filterByConstrainedBy("someSchema")
+
+// Fetch all Sparql View instances on a Project
+const myViews: SparqlView[] = await myProject.listSparqlViews();
+
+// Query a Project with Sparql
+const response: SparqlQueryViewResponse = myView.query('SELECT * where {?s ?p ?o} LIMIT 50');
 ```
 
 # License
--- nexus-sdk-js/src/Nexus.ts
+++ nexus-sdk-js/src/Nexus.ts
@@ -2,9 +2,6 @@ import { httpGet, httpPut } from './utils/http';
 import Store from './utils/Store';
 import Organization, { ListOrgsResponse, OrgResponse } from './Organization';
 import { CreateOrganizationException } from './Organization/exceptions';
-// import Project from './Project';
-// import SparqlView from './views/SparqlView';
-// import ElasticSeachView from './views/ElasticSearchView';
 
 type NexusConfig = {
   environment: string;
@@ -120,48 +117,3 @@ export default class Nexus {
     }
   }
 }
-
-// // Debugggg
-// const nexus = new Nexus({
-//   // environment: 'https://bbp-nexus.epfl.ch/staging/v1',
-//   environment: 'https://bbp.epfl.ch/nexus/v1',
-//   // token: ''
-// });
-
-// // Promise.all([
-// //   nexus
-// //     .getOrganization('kenny')
-// //     .then((org: Organization) => {
-// //       console.log(org.id);
-// //       return org.getProject('search');
-// //     })
-// //     .then((project: Project) => {
-// //       console.log(project.id);
-// //       return project.listSparqlViews();
-// //     })
-// //     .then((svs: SparqlView[]) => {
-// //       console.log(svs);
-// //       return svs[0].query('SELECT ?A ?edge ?B where {?A ?edge ?B} LIMIT 50');
-// //     })
-// //     .then(r => console.log(r))
-// //     .catch(e => console.error(e)),
-// // ]);
-
-// Promise.all([
-//   nexus
-//     .getOrganization('anorg')
-//     .then((org: Organization) => {
-//       console.log(org.id);
-//       return org.getProject('testcore');
-//     })
-//     .then((project: Project) => {
-//       console.log(project.id);
-//       return project.listElasticSearchViews();
-//     })
-//     .then((evs: ElasticSeachView[]) => {
-//       console.log(evs);
-//       return evs[0].filterByTypes(['http://schema.org/Person']);
-//     })
-//     .then(r => console.log(JSON.stringify(r, null, 2)))
-//     .catch(e => console.error(e)),
-// ]);

1658ebcce924afdcfacc429c0794a1973633e736
Dupont Michaël <michael.dupont@epfl.ch>
2019-01-14 17:16:39 +0100

    Removed leftover imports

--- nexus-sdk-js/src/index.ts
+++ nexus-sdk-js/src/index.ts
@@ -4,7 +4,7 @@ export { default as Nexus } from './Nexus';
 export { default as Organization } from './Organization';
 export { default as Project } from './Project';
 export { default as Resource } from './Resource';
-export { default as ElasticSearchView, SingleElasticSearchView, AggregateElasticSearchView } from './views/ElasticSearchView';
+export { default as ElasticSearchView } from './views/ElasticSearchView';
 export { default as SparqlView } from './views/SparqlView';
 export { default as ACL } from './ACL';
 export * from './utils/types';

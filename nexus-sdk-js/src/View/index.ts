79dd72bca05c31be638570ed97a72070f7d4e31f
Julien Machon <julienmachon@gmail.com>
2019-01-17 11:27:47 +0100

    cannot export TS interfaces as they don't exists after transpiling the code

--- nexus-sdk-js/src/View/index.ts
+++ nexus-sdk-js/src/View/index.ts
@@ -1,7 +1,7 @@
 import { getView, listViews } from './utils';
 
-export { default as ElasticSearchView, ElasticSearchViewQueryResponse } from './ElasticSearchView';
-export { default as SparqlView, SparqlViewQueryResponse } from './SparqlView';
+export { default as ElasticSearchView } from './ElasticSearchView';
+export { default as SparqlView } from './SparqlView';
 
 export default class View {
   static get = getView;

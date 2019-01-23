6d8d2ae24e01f61ec3e810b56efc1c0faf9ced31
Julien Machon <julienmachon@gmail.com>
2019-01-23 12:18:22 +0100

    Initial creation of realm class and types

---
+++ nexus-sdk-js/src/Realm/index.ts
@@ -0,0 +1,41 @@
+import { RealmResponse } from './types';
+
+export default class Realm {
+  context?: string | string[];
+  id: string;
+  type: string;
+  name: string;
+  openidConfig: string;
+  label: string;
+  issuer: string;
+  authorizationEndpoint: string;
+  tokenEndpoint: string;
+  userInfoEndpoint: string;
+  endSessionEnpoint: string;
+  createdAt: string;
+  createdBy: string;
+  updatedAt: string;
+  updatedBy: string;
+  rev: number;
+  deprecated: boolean;
+
+  constructor(realmResponse: RealmResponse) {
+    this.context = realmResponse['@context'];
+    this.id = realmResponse['@id'];
+    this.type = realmResponse['@type'];
+    this.name = realmResponse.name;
+    this.openidConfig = realmResponse.openidConfig;
+    this.label = realmResponse._label;
+    this.issuer = realmResponse._issuer;
+    this.authorizationEndpoint = realmResponse._authorizationEndpoint;
+    this.tokenEndpoint = realmResponse._tokenEndpoint;
+    this.userInfoEndpoint = realmResponse._userInfoEndpoint;
+    this.endSessionEnpoint = realmResponse._endSessionEndpoint;
+    this.createdAt = realmResponse._createdAt;
+    this.createdBy = realmResponse._createdBy;
+    this.updatedAt = realmResponse._updatedAt;
+    this.updatedBy = realmResponse._updatedBy;
+    this.rev = realmResponse._rev;
+    this.deprecated = realmResponse._deprecated;
+  }
+}
---
+++ nexus-sdk-js/src/Realm/types.ts
@@ -0,0 +1,32 @@
+export type Context = string | string[];
+
+export interface RealmResponseCommon {
+  '@id': string;
+  '@type': string;
+  name: string;
+  openidConfig: string;
+  _label: string;
+  _issuer: string;
+  _authorizationEndpoint: string;
+  _tokenEndpoint: string;
+  _userInfoEndpoint: string;
+  _endSessionEndpoint: string;
+  _createdAt: string;
+  _createdBy: string;
+  _updatedAt: string;
+  _updatedBy: string;
+  _rev: number;
+  _deprecated: boolean;
+}
+export interface RealmResponse extends RealmResponseCommon {
+  '@context': Context;
+}
+export interface ListRealmResponse {
+  '@context': Context;
+  _total: number;
+  _results: RealmResponseCommon[];
+}
+
+export interface ListRealmOptions {
+  deprecated: boolean;
+}

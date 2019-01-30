60cdac9c5544725c0866d94d2d0dda6bee2ce340
Julien Machon <julienmachon@gmail.com>
2019-01-30 10:13:58 +0100

    adding query params to request

--- nexus-sdk-js/src/ACL/utils.ts
+++ nexus-sdk-js/src/ACL/utils.ts
@@ -1,16 +1,22 @@
 import ACL from '.';
 import { ACLResponse } from './types';
 import { httpGet } from '../utils/http';
+import { buildQueryParams } from '../utils';
 
 export async function getACL(
   path: string,
-  rev?: number,
-  self?: boolean,
+  options?: {
+    rev?: number;
+    self?: boolean;
+  },
 ): Promise<ACL> {
   try {
-    const aclResponse: ACLResponse = await httpGet(`/acls/${path}`);
+    const opts = buildQueryParams(options);
+    const aclResponse: ACLResponse = await httpGet(`/acls/${path}${opts}`);
     return new ACL(aclResponse);
   } catch (error) {
     throw error;
   }
 }
+
+getACL('asd');

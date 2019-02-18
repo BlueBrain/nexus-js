31c3ada0214d1e0745ca2c1934da1cade81be685
kenjinp <github@kenny.wtf>
2019-02-18 21:42:49 +0100

    FileResponse

---
+++ nexus-sdk-js/src/File/types.ts
@@ -0,0 +1,23 @@
+import { Context } from '../Resource/types';
+
+export interface FileResponse {
+  '@context': Context;
+  '@id': string;
+  '@type': 'File';
+  _bytes: number;
+  _digest: {
+    _algorithm: string;
+    _value: string;
+  };
+  _filename: string;
+  _mediaType: string;
+  _self: string;
+  _constrainedBy: string;
+  _project: string;
+  _rev: 1;
+  _deprecated: false;
+  _createdAt: Date;
+  _createdBy: string;
+  _updatedAt: Date;
+  _updatedBy: string;
+}

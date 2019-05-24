5fb332a105e2c380c184ed2d6214c4f8d2e16e3a
Julien Machon <julienmachon@gmail.com>
2019-05-24 16:32:44 +0200

    tag and deprecate

--- packages/nexus-sdk/src/View/view.ts
+++ packages/nexus-sdk/src/View/view.ts
@@ -2,8 +2,12 @@ import { toPromise, Observable } from '@bbp/nexus-link';
 import { Fetchers } from '../types';
 import { NexusContext } from '../nexusSdk';
 import { View, ViewList } from './types';
+import { buildQueryParams } from '../utils';
 
-const View = ({ httpGet, poll }: Fetchers, context: NexusContext) => {
+const View = (
+  { httpGet, httpPost, httpDelete, poll }: Fetchers,
+  context: NexusContext,
+) => {
   return {
     get: <T = View>(
       orgLabel: string,
@@ -25,8 +29,45 @@ const View = ({ httpGet, poll }: Fetchers, context: NexusContext) => {
           }/views/${orgLabel}/${projectLabel}`,
         }),
       ),
-    tag: () => {},
-    deprecate: () => {},
+    tag: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      payload: {
+        tag: string;
+        rev: number;
+      },
+      options?: {
+        rev: number;
+      },
+    ): Promise<View> => {
+      const opts = buildQueryParams(options);
+      return toPromise(
+        httpPost({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}${opts}`,
+          body: JSON.stringify(payload),
+        }),
+      );
+    },
+    deprecate: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      options?: {
+        rev: number;
+      },
+    ): Promise<View> => {
+      const opts = buildQueryParams(options);
+      return toPromise(
+        httpDelete({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}${opts}`,
+        }),
+      );
+    },
     poll: <T = View>(
       orgLabel: string,
       projectLabel: string,

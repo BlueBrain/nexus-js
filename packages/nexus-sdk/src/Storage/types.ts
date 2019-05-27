6af36f7aa4375825c082f7c0b6a005b0acda5cd1
Julien Machon <julienmachon@gmail.com>
2019-05-27 10:36:55 +0200

    Implement storage (#134)
    * types and methods implemented
    
    * minor changes on Resolvers
    
    * expose storage
    
    * TODO: define types properly
    
    * add types

---
+++ packages/nexus-sdk/src/Storage/types.ts
@@ -0,0 +1,82 @@
+import { Resource, PaginatedResource } from '../types';
+
+export type StorageType = 'DiskStorage' | 'RemoteDiskStorage' | 'S3Storage';
+
+export type Storage = DiskStorage | RemoteDiskStorage | S3Storage;
+export type DiskStorage = Resource & {
+  '@type': ['DiskStorage', 'Storage'];
+  volume: string;
+  default: boolean;
+  readPermissions?: string[];
+  writePermissions?: string[];
+  _algorithm: string;
+};
+export type RemoteDiskStorage = Resource & {
+  '@type': ['RemoteStorage', 'Storage'];
+  folder: string;
+  default: boolean;
+  endpoint?: string;
+  readPermissions?: string[];
+  writePermissions?: string[];
+  _algorithm: string;
+};
+export type S3Storage = Resource & {
+  '@type': ['S3Storage', 'Storage'];
+  default: boolean;
+  readPermissions?: string[];
+  writePermissions?: string[];
+  bucket: string;
+  _algorithm: string;
+};
+export type StorageList = PaginatedResource<Storage>;
+
+export type GetStorageOptions = {
+  rev?: number;
+  tag?: string;
+};
+export type ListStorageOptions = {
+  from?: number;
+  size?: number;
+  deprecated?: boolean;
+  rev?: number;
+  type?: StorageType;
+  createdBy?: string;
+  updatedBy?: string;
+};
+
+export type StoragePayload =
+  | DiskStoragePayload
+  | RemoteDiskStoragePayload
+  | S3StoragePayload;
+
+export type DiskStoragePayload = {
+  '@id': string;
+  '@tpye': ['DiskStorage'];
+  volume: string;
+  default?: boolean;
+  readPermissions?: string[];
+  writePermissions?: string[];
+};
+
+export type RemoteDiskStoragePayload = {
+  '@id': string;
+  '@type': ['RemoteDiskStorage'];
+  folder: string;
+  default?: boolean;
+  endpoint?: string;
+  credentials: string;
+  readPermissions?: string[];
+  writePermissions?: string[];
+};
+
+export type S3StoragePayload = {
+  '@id': string;
+  '@type': ['S3Storage'];
+  bucket: string;
+  default?: boolean;
+  endpoint?: string;
+  accessKey?: string;
+  secretKey?: string;
+  readPermissions?: string[];
+  writePermissions?: string[];
+};

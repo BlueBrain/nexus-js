2f6f47bc2b75e38c3291adc3b58377ccef6c5971
Julien Machon <julienmachon@gmail.com>
2019-05-27 14:10:54 +0200

    simplify CRUD (#137)
    * simplify CRUD
    
    * Write ops return Resource, alway
    
    * remove useless parameterised tpye

--- packages/nexus-sdk/src/View/index.ts
+++ packages/nexus-sdk/src/View/index.ts
@@ -1,7 +1,144 @@
-export { default as View } from './view';
-export { default as SparqlView } from './sparqlView';
-export { default as ElasticSearchView } from './elasticSearchView';
-export {
-  default as AggregatedElasticSearchView,
-} from './aggregatedElasticSearchView';
-export { default as AggregatedSparqlView } from './aggregatedSparqlView';
+import { toPromise, Observable } from '@bbp/nexus-link';
+import { Fetchers, Resource } from '../types';
+import { NexusContext } from '../nexusSdk';
+import { View, ViewList, ViewPayload } from './types';
+import { buildQueryParams } from '../utils';
+
+const View = (
+  { httpGet, httpPost, httpPut, httpDelete, poll }: Fetchers,
+  context: NexusContext,
+) => {
+  return {
+    get: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+    ): Promise<View> =>
+      toPromise(
+        httpGet({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}`,
+        }),
+      ),
+    list: (orgLabel: string, projectLabel: string): Promise<ViewList> =>
+      toPromise(
+        httpGet({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}`,
+        }),
+      ),
+    create: (
+      orgLabel: string,
+      projectLabel: string,
+      payload: ViewPayload,
+    ): Promise<Resource> =>
+      toPromise(
+        httpPut({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}`,
+          body: JSON.stringify(payload),
+        }),
+      ),
+    update: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      rev: number,
+      payload: ViewPayload,
+    ): Promise<Resource> =>
+      toPromise(
+        httpPut({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}?rev=${rev}`,
+          body: JSON.stringify(payload),
+        }),
+      ),
+    tag: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      rev: number,
+      payload: {
+        tag: string;
+        rev: number;
+      },
+    ): Promise<Resource> =>
+      toPromise(
+        httpPost({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}?rev=${rev}`,
+          body: JSON.stringify(payload),
+        }),
+      ),
+    deprecate: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      rev: number,
+    ): Promise<Resource> =>
+      toPromise(
+        httpDelete({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}?rev=${rev}`,
+        }),
+      ),
+    poll: (
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      options?: { pollTime: number },
+    ): Observable<View> =>
+      poll({
+        path: `${context.uri}/${
+          context.version
+        }/views/${orgLabel}/${projectLabel}/${viewId}`,
+        context: { pollTime: options && options.pollTime | 1000 },
+      }),
+    elasticSearchQuery: <T = any>(
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      query: {},
+      options?: {
+        from: number;
+        size: number;
+      },
+    ): Promise<T> => {
+      const opts = buildQueryParams(options);
+      return toPromise(
+        httpPost({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}/_search${opts}`,
+          body: JSON.stringify(query),
+        }),
+      );
+    },
+    sparqlQuery: <T = any>(
+      orgLabel: string,
+      projectLabel: string,
+      viewId: string,
+      query: {},
+    ): Promise<T> => {
+      return toPromise(
+        httpPost({
+          path: `${context.uri}/${
+            context.version
+          }/views/${orgLabel}/${projectLabel}/${viewId}/sparql`,
+          body: JSON.stringify(query),
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+        }),
+      );
+    },
+  };
+};
+
+export default View;

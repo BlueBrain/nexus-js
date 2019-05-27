59331dc061396f440bc75e50b24870cc0d26487b
Julien Machon <julienmachon@gmail.com>
2019-05-27 10:40:24 +0200

    Implement schemas (#135)
    * types and methods
    
    * expose schema
    
    * add tpyes

---
+++ packages/nexus-sdk/src/Schema/index.ts
@@ -0,0 +1,99 @@
+import { toPromise, Observable } from '@bbp/nexus-link';
+import { Fetchers } from '../types';
+import {
+  GetSchemaOptions,
+  ListSchemaOptions,
+  SchemaList,
+  SchemaPayload,
+} from './types';
+import { NexusContext } from '../nexusSdk';
+import { buildQueryParams } from '../utils';
+
+const Schema = (
+  { httpGet, httpPut, httpDelete, poll }: Fetchers,
+  context: NexusContext,
+) => {
+  return {
+    get: (
+      orgLabel: string,
+      projectLabel: string,
+      schemaId: string,
+      options?: GetSchemaOptions,
+    ): Promise<Storage> => {
+      const opts = buildQueryParams(options);
+      return toPromise(
+        httpGet({
+          path: `${context.uri}/${
+            context.version
+          }/schemas/${orgLabel}/${projectLabel}/${schemaId}${opts}`,
+        }),
+      );
+    },
+    list: (
+      orgLabel?: string,
+      options?: ListSchemaOptions,
+    ): Promise<SchemaList> => {
+      const opts = buildQueryParams(options);
+      return toPromise(
+        httpGet({
+          path: `${context.uri}/${context.version}/schemas/${orgLabel}${opts}`,
+        }),
+      );
+    },
+    create: (
+      orgLabel: string,
+      projectLabel: string,
+      payload: SchemaPayload,
+    ): Promise<Storage> =>
+      toPromise(
+        httpPut({
+          path: `${context.uri}/${
+            context.version
+          }/schemas/${orgLabel}/${projectLabel}`,
+          body: JSON.stringify(payload),
+        }),
+      ),
+    update: (
+      orgLabel: string,
+      projectLabel: string,
+      schemaId: string,
+      rev: number,
+      payload: SchemaPayload,
+    ): Promise<Storage> =>
+      toPromise(
+        httpPut({
+          path: `${context.uri}/${
+            context.version
+          }/schemas/${orgLabel}/${projectLabel}/${schemaId}?rev=${rev}`,
+          body: JSON.stringify(payload),
+        }),
+      ),
+    deprecate: (
+      orgLabel: string,
+      projectLabel: string,
+      schemaId: string,
+      rev: number,
+    ): Promise<Storage> =>
+      toPromise(
+        httpDelete({
+          path: `${context.uri}/${
+            context.version
+          }/schemas/${orgLabel}/${projectLabel}/${schemaId}?rev=${rev}`,
+        }),
+      ),
+    poll: (
+      orgLabel: string,
+      projectLabel: string,
+      schemasId: string,
+      options?: { pollTime: number },
+    ): Observable<Storage> =>
+      poll({
+        path: `${context.uri}/${
+          context.version
+        }/schemas/${orgLabel}/${projectLabel}/${schemasId}`,
+        context: { pollTime: options && options.pollTime | 1000 },
+      }),
+  };
+};
+
+export default Schema;

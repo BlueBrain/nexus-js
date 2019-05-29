6ddcc5a56d744617a5fc704d8ca5d6d23a44a04f
Julien Machon <julienmachon@gmail.com>
2019-05-29 12:20:35 +0200

    sample test for file

--- packages/nexus-sdk/src/File/__tests__/File.spec.ts
+++ packages/nexus-sdk/src/File/__tests__/File.spec.ts
@@ -3,18 +3,27 @@ import { mockFetchers } from '../../testUtils';
 
 const file = NexusFile(mockFetchers, { uri: 'http://api.url', version: 'v1' });
 
-describe('testing api', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
+describe('KG', () => {
+  describe('File', () => {
+    beforeEach(() => {
+      fetchMock.resetMocks();
+    });
 
-  it('calls google and returns data to me', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({ data: '' }));
-    await file.list('org', 'project');
-    //assert on the times called and arguments given to fetch
-    expect(fetchMock.mock.calls.length).toEqual(1);
-    expect(fetchMock.mock.calls[0][0]).toEqual(
-      'http://api.url/v1/files/org/project',
-    );
+    it('should make httpGet call to the file api', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ data: '' }));
+      await file.list('org', 'project');
+      expect(fetchMock.mock.calls.length).toEqual(1);
+      expect(fetchMock.mock.calls[0][0]).toEqual(
+        'http://api.url/v1/files/org/project',
+      );
+    });
+    it('should make httpGet with query params', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ data: '' }));
+      await file.list('org', 'project', { createdBy: 'me' });
+      expect(fetchMock.mock.calls.length).toEqual(1);
+      expect(fetchMock.mock.calls[0][0]).toEqual(
+        'http://api.url/v1/files/org/project?createdBy=me',
+      );
+    });
   });
 });

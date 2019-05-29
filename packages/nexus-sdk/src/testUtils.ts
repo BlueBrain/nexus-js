593774a9e27e104d73e0ae98f3359f35ea931455
Julien Machon <julienmachon@gmail.com>
2019-05-29 15:33:09 +0200

    fix mockFetchers signatures

--- packages/nexus-sdk/src/testUtils.ts
+++ packages/nexus-sdk/src/testUtils.ts
@@ -2,10 +2,15 @@ import { Operation, Observable } from '@bbp/nexus-link';
 import { Fetchers } from './types';
 
 export const mockFetchers: Fetchers = {
-  httpGet: (operation: Operation) => fetch(operation.path, { ...operation }),
-  httpPost: (operation: Operation) => fetch(operation.path, { ...operation }),
-  httpPut: (operation: Operation) => fetch(operation.path, { ...operation }),
-  httpPatch: (operation: Operation) => fetch(operation.path, { ...operation }),
-  httpDelete: (operation: Operation) => fetch(operation.path, { ...operation }),
+  httpGet: (operation: Operation) =>
+    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+  httpPost: (operation: Operation) =>
+    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+  httpPut: (operation: Operation) =>
+    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+  httpPatch: (operation: Operation) =>
+    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+  httpDelete: (operation: Operation) =>
+    fetch(operation.path, { headers: operation.headers, body: operation.body }),
   poll: (operation: Operation) => new Observable(() => {}),
 };

788f3910f5cd376f3dc4d2b77002714325db24b1
Julien Machon <julienmachon@gmail.com>
2019-05-29 17:34:03 +0200

    set method on mock http

--- packages/nexus-sdk/src/testUtils.ts
+++ packages/nexus-sdk/src/testUtils.ts
@@ -3,14 +3,34 @@ import { Fetchers } from './types';
 
 export const mockFetchers: Fetchers = {
   httpGet: (operation: Operation) =>
-    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+    fetch(operation.path, {
+      headers: operation.headers,
+      body: operation.body,
+      method: 'GET',
+    }),
   httpPost: (operation: Operation) =>
-    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+    fetch(operation.path, {
+      headers: operation.headers,
+      body: operation.body,
+      method: 'POST',
+    }),
   httpPut: (operation: Operation) =>
-    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+    fetch(operation.path, {
+      headers: operation.headers,
+      body: operation.body,
+      method: 'PUT',
+    }),
   httpPatch: (operation: Operation) =>
-    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+    fetch(operation.path, {
+      headers: operation.headers,
+      body: operation.body,
+      method: 'PATCH',
+    }),
   httpDelete: (operation: Operation) =>
-    fetch(operation.path, { headers: operation.headers, body: operation.body }),
+    fetch(operation.path, {
+      headers: operation.headers,
+      body: operation.body,
+      method: 'DELETE',
+    }),
   poll: (operation: Operation) => new Observable(() => {}),
 };

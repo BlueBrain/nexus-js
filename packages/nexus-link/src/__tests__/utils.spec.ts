a815cd419a69214322a16d7bd91ca74c8b4afa29
Kenneth Pirman <github@kenny.wtf>
2019-05-31 11:09:01 +0200

    Update utils.spec.ts

--- packages/nexus-link/src/__tests__/utils.spec.ts
+++ packages/nexus-link/src/__tests__/utils.spec.ts
@@ -1,5 +1,13 @@
-describe('something', () => {
-  it('should', () => {
-    expect(1).toEqual(1);
+import * as utils from '../utils';
+import Observable from 'zen-observable-ts';
+
+describe('toPromise', () => {
+  it('should convery an Observable to a Promise', async () => {
+    const obs = new Observable(subscribe => {
+      subscribe.next(1);
+    });
+    const value = await utils.toPromise(obs);
+
+    expect(value).toBe(1);
   });
 });

771d870c72befddf8d3a9deb1769f32dc804ca11
Julien Machon <julienmachon@gmail.com>
2019-05-31 11:18:36 +0200

    shared config (#164)
    * shared config
    
    * add yaml codecov from Kenny's PR
    
    * debug stupid jenkins test failure
    
    * Update utils.spec.ts
    
    * fix silly lint problem
    
    * put badge on root readme

--- jest.config.base.js
+++ jest.config.base.js
@@ -5,5 +5,6 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  setupFiles: [__dirname + '/scripts/setupJest.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
--- packages/nexus-link/jest.config.js
+++ packages/nexus-link/jest.config.js
@@ -4,5 +4,4 @@ module.exports = {
   ...base,
   name: '@bbp/nexus-link',
   displayName: '@bbp/nexus-link',
-  setupFiles: ['../../scripts/setupJest.ts'],
 };

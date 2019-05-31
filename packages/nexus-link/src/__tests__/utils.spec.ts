bde22084f1387eb66c40601961cbf40f773d429f
kenjinp <github@kenny.wtf>
2019-05-31 11:06:45 +0200

    make a simple test

--- packages/nexus-link/src/__tests__/utils.spec.ts
+++ packages/nexus-link/src/__tests__/utils.spec.ts
@@ -1,5 +1,19 @@
+import * as utils from '../utils';
+import Observable from 'zen-observable-ts';
+
 describe('something', () => {
   it('should', () => {
     expect(1).toEqual(1);
+  });
+});
+
+describe('toPromise', () => {
+  it('should conver stuff to a promise', async () => {
+    const obs = new Observable(subscribe => {
+      subscribe.next(1);
+    });
+    const value = await utils.toPromise(obs);
+
+    expect(value).toBe(1);
   });
 });

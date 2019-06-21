b1571e48953ba000c08f547ae43575b116ba1d68
kenjinp <github@kenny.wtf>
2019-06-21 17:25:32 +0200

    add projects

--- packages/react-nexus/src/index.ts
+++ packages/react-nexus/src/index.ts
@@ -1,4 +1,5 @@
 export { default as NexusProvider } from './NexusProvider';
 export { default as useNexus } from './useNexus';
 export { default as Orgs } from './Orgs';
+export { default as Projects } from './Projects';
 export * from './AccessControl';

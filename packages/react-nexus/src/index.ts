8c32844dd2f9d59803601cb6c4a725e23285f2a9
Julien Machon <julienmachon@gmail.com>
2019-07-04 15:00:57 +0200

    useNexusContext export

--- packages/react-nexus/src/index.ts
+++ packages/react-nexus/src/index.ts
@@ -1,4 +1,4 @@
 export { default as NexusProvider } from './NexusProvider';
 export { default as useNexus } from './useNexus';
-export { default as nexusContext } from './nexusContext';
+export { default as useNexusContext } from './useNexusContext';
 export * from './AccessControl';

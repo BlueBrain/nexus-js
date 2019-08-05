e93fdd70ffe90d37ea67b745c9783bc6a8639e3c
Kenneth Pirman <github@kenny.wtf>
2019-08-05 13:38:18 +0200

    fix: rename Details to ResourceDetails to be more specific (#211)

---
+++ apps/search-poc/src/components/ResourceDetails.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+const ResourceDetails: React.FunctionComponent<{
+  id: string;
+  name: string;
+  description: string;
+  brainRegion: {
+    id: string;
+    label: string;
+  };
+}> = props => {
+  return (
+    <div className="Details">
+      <h1>Common entity details</h1>
+
+      <div>
+        <p>Id: {props.id}</p>
+        <p>
+          Brain region: &nbsp;
+          <a
+            href={props.brainRegion.id}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {props.brainRegion.label}
+          </a>
+        </p>
+        <p>Name: {props.name}</p>
+        <p>Description: {props.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResourceDetails;

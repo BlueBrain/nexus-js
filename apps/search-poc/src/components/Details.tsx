e3d6f389f030119edd87ce3f45be3911fd49c6fa
Pavlo Getta <pavlo.getta@epfl.ch>
2019-08-05 11:06:00 +0200

    Details page: move error and loading state handling to container

--- apps/search-poc/src/components/Details.tsx
+++ apps/search-poc/src/components/Details.tsx
@@ -1,63 +1,34 @@
 import * as React from "react";
-import { Spin } from 'antd';
-import get from 'lodash/get';
-
-
-export interface BrainRegion {
-  '@id': string;
-  label: string;
-}
-
-export interface BrainLocation {
-  brainRegion: BrainRegion;
-}
-
-export interface CommonResource {
-  '@id': string;
-  brainLocation: BrainLocation;
-  name: string;
-  description: string;
-}
 
 
 const DetailsComponent: React.FunctionComponent<{
-  data: CommonResource;
-  loading: boolean;
-  error: Error;
+  id: string;
+  name: string;
+  description: string;
+  brainRegion: {
+    id: string;
+    label: string;
+  };
 }> = props => {
-  const { data, loading, error } = props;
-
-  const id = get(data, '@id');
-  const name = get(data, 'name');
-  const description = get(data, 'description');
-
-  const brainRegion = get(data, 'brainLocation.brainRegion', {});
-  const brainRegionId = brainRegion['@id'];
-  const brainRegionLabel = brainRegion.label;
-
   return (
-    <div className={error ? 'details error' : 'details'}>
-      <Spin tip="Loading..." spinning={loading}>
+    <div className="Details">
         <h1>Common entity details</h1>
 
-        {error && <p>{error.message}</p>}
-
-        {data && <div>
-          <p>Id: {id}</p>
+        <div>
+          <p>Id: {props.id}</p>
           <p>
             Brain region: &nbsp;
             <a
-              href={brainRegionId}
+              href={props.brainRegion.id}
               rel="noopener noreferrer"
               target="_blank"
             >
-              {brainRegionLabel}
+              {props.brainRegion.label}
             </a>
           </p>
-          <p>Name: {name}</p>
-          <p>Description: {description}</p>
-        </div>}
-      </Spin>
+          <p>Name: {props.name}</p>
+          <p>Description: {props.description}</p>
+        </div>
     </div>
   );
 };
--- apps/search-poc/src/containers/Details.tsx
+++ apps/search-poc/src/containers/Details.tsx
@@ -1,18 +1,56 @@
 import * as React from 'react';
+import get from 'lodash/get';
 import { useNexus } from '@bbp/react-nexus';
+import { Spin } from 'antd';
 
 import DetailsComponent from '../components/Details';
+import { Resource } from '@bbp/nexus-sdk';
+
+export interface BrainRegion {
+  '@id': string;
+  label: string;
+}
+
+export interface BrainLocation {
+  brainRegion: BrainRegion;
+}
+
+export interface MINDSResourse {
+  brainLocation: BrainLocation;
+  name: string;
+  description: string;
+}
 
 
 const DetailsContainer: React.FunctionComponent<{
   selfUrl: string;
 }> = props => {
-  const { data, loading, error } = useNexus(nexus => nexus.httpGet({ path: props.selfUrl }));
+  const { data, loading, error } = useNexus<Resource & MINDSResourse>(nexus => nexus.httpGet({ path: props.selfUrl }));
+
+  const id = get(data, '@id');
+  const name = get(data, 'name');
+  const description = get(data, 'description');
+
+  const brainRegionId = get(data, 'brainLocation.brainRegion.@id');
+  const brainRegionLabel = get(data, 'brainLocation.brainRegion.label');
+  const brainRegion = {
+    id: brainRegionId,
+    label: brainRegionLabel,
+  };
+
+  if (loading) {
+    return <Spin></Spin>
+  }
+
+  if (error) {
+    return <p>{error.message}</p>
+  }
 
   return <DetailsComponent
-    data={data}
-    loading={loading}
-    error={error}
+    id={id}
+    name={name}
+    description={description}
+    brainRegion={brainRegion}
   />
 };
 

d223ae129d51df9c4874d385efd953c5acd7f2d7
Pavlo Getta <pgetta@users.noreply.github.com>
2019-08-15 15:57:41 +0200

    EModel details component (#227)
    * Add emodel details component
    
    * Rename params table header
    
    * Use named import foor LazyExoticComponent
    
    * Refactor EModel component
    
    * extend fitness table with recType row
    * move fitness column config into the component
    
    * Move common styles to global styles
    
    * Fix CI

---
+++ apps/search-poc/src/components/ResourceDetails/EModel.tsx
@@ -0,0 +1,118 @@
+
+import React, { ReactNode } from 'react';
+import sortBy from 'lodash/sortBy';
+import { Table, Collapse } from 'antd';
+
+import { Resource } from '@bbp/nexus-sdk';
+import { MINDSResource, EModelResource } from '../../containers/ResourceDetails/types';
+
+
+const { Panel } = Collapse;
+const protocolR = /^(?:_\.)(.*)\.(\w+)\.(\w+)\.(\w+)$/;
+
+const paramColumns = [{
+  title: 'Parameter',
+  dataIndex: 'param',
+  key: 'param',
+}, {
+  title: 'Value',
+  dataIndex: 'val',
+  key: 'val',
+}];
+
+function createUniqColumnRenderer(
+  dataSource: object[],
+  dataKey: string,
+  colKey: string
+): (value: any, row: any, index: number) => ReactNode | undefined {
+  return (value, row, index) => {
+    const obj = { children: value[dataKey], props: { rowSpan: 1 } };
+    const isRendered = dataSource
+      .slice(0, index)
+      .some(entry => entry[colKey] === value[colKey]);
+
+    obj.props.rowSpan = isRendered
+      ? 0
+      : dataSource.filter(entry => entry[colKey] === value[colKey]).length;
+
+    return obj;
+  };
+};
+
+function createFitnessDataSourceEntry([paramRaw, val]) {
+  const [, protocolRaw, recTarget, recType, param ] = paramRaw
+    .match(protocolR) as string[];
+
+  const protocol = protocolRaw.replace(/_/g, ' ');
+  const key = paramRaw;
+
+  return { key, protocol, recTarget, recType, param, val };
+};
+
+const EModelDetails: React.FunctionComponent<{
+  resource: Resource & MINDSResource & EModelResource;
+}> = props => {
+  const { score, seed, fitness, params } = props.resource;
+
+  const fitnessDSUnsorted = Object.entries(fitness)
+    .map(createFitnessDataSourceEntry);
+
+  const fitnessDataSource = sortBy(fitnessDSUnsorted, ['parameter', 'recTarget', 'protocol']);
+
+  const fitnessColumns = [{
+    title: 'Protocol',
+    render: createUniqColumnRenderer(fitnessDataSource, 'protocol', 'protocol'),
+    key: 'protocol',
+  }, {
+    title: 'Target',
+    render: createUniqColumnRenderer(fitnessDataSource, 'recTarget', 'protocol'),
+    key: 'recTarget',
+  }, {
+    title: 'Type',
+    render: createUniqColumnRenderer(fitnessDataSource, 'recType', 'protocol'),
+    key: 'recType'
+  }, {
+    title: 'Parameter',
+    dataIndex: 'param',
+    key: 'param',
+  }, {
+    title: 'Value',
+    dataIndex: 'val',
+    key: 'val',
+  }];
+
+  const paramDataSource = Object.entries(params)
+    .map(([key, val]) => ({ key, param: key, val }));
+
+  return (
+    <div className="emodel-details">
+      <p>Seed: {seed}</p>
+      <p>Score: {score}</p>
+
+      <Collapse>
+        <Panel header="Fitness" key="fitness">
+          <Table
+            className="small-table"
+            dataSource={fitnessDataSource}
+            columns={fitnessColumns}
+            pagination={false}
+            size="middle"
+            bordered
+          />
+        </Panel>
+        <Panel header="Optimized parameters" key="params">
+          <Table
+            className="small-table"
+            dataSource={paramDataSource}
+            columns={paramColumns}
+            pagination={false}
+            size="middle"
+            bordered
+          />
+        </Panel>
+      </Collapse>
+    </div>
+  );
+};
+
+export default EModelDetails;
--- apps/search-poc/src/containers/ResourceDetails/index.ts
+++ apps/search-poc/src/containers/ResourceDetails/index.ts
@@ -1,22 +1,25 @@
 
-import React from 'react';
+import { lazy, LazyExoticComponent } from 'react';
 
 
-const MorphViewer = React.lazy(() => import('./ReconstructedNeuronMorphology'));
+const MorphViewer = lazy(() => import('./ReconstructedNeuronMorphology'));
+const EModelDetails = lazy(() => import('../../components/ResourceDetails/EModel'));
 
 const detailsComponents = {
   morphViewer: MorphViewer,
+  emodelDetails: EModelDetails,
 };
 
 const compNamesByType = {
   ReconstructedNeuronMorphology: ['morphViewer'],
+  EModel: ['emodelDetails'],
 }
 
 
 // Get matching components by a list of resource types.
 // Returns an object with component names as keys and components as their values.
 export function getComponentsForTypes(resourceTypes: string[]): {
-  [componentName: string]: React.LazyExoticComponent<any>
+  [componentName: string]: LazyExoticComponent<any>
 }[] {
   return resourceTypes
     .flatMap(type => compNamesByType[type] || [])

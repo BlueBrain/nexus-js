40d99d75916c1d02751b016d4824fcf861fede41
Pavlo Getta <pavlo.getta@epfl.ch>
2019-08-16 14:34:40 +0200

    Add silent auth renew

--- apps/search-poc/src/utils/auth.ts
+++ apps/search-poc/src/utils/auth.ts
@@ -12,8 +12,14 @@ export function getConfig(realm?: Realm) {
     popup_redirect_uri: SETTINGS.redirectUrl,
     post_logout_redirect_uri: SETTINGS.redirectUrl,
     response_type: 'id_token token',
+    automaticSilentRenew: true,
+    silent_redirect_uri: `${window.location.origin}/silent-auth-callback.html`,
     loadUserInfo: true,
   };
+}
+
+function saveAccessToken(token: string) {
+  localStorage.setItem(SETTINGS.bearerTokenKey, token);
 }
 
 // link that sets the bearer token before each nexus call
@@ -39,8 +45,6 @@ export const setToken: Link = (operation: Operation, forward?: Link) => {
  * - return a ref of user manager instance and the user
  */
 export async function setUpSession(): Promise<[UserManager, User | null]> {
-  let user: User | null = null;
-  let userManager: UserManager | null = null;
   // Get preferred realm
   const preferredRealm: Realm = JSON.parse(
     localStorage.getItem(SETTINGS.preferredRealmKey) || '{}',
@@ -48,24 +52,24 @@ export async function setUpSession(): Promise<[UserManager, User | null]> {
 
   // then create User Manager with preferred realm issuer
   // or empty user manager if no preferred realm
-  userManager = new UserManager(getConfig(preferredRealm));
+  const userManager: UserManager | null = new UserManager(
+    getConfig(preferredRealm),
+  );
 
   // maybe we're getting new user session after redirect
   try {
-    user = await userManager.signinRedirectCallback();
+    await userManager.signinRedirectCallback();
   } catch (error) {}
   // if not, check if we already had a running session
-  if (!user) {
-    try {
-      user = await userManager.getUser();
-    } catch (error) {
-      // if not, that's fine
-    }
+  const user: User | null = await userManager.getUser();
+
+  if (user) {
+    saveAccessToken(user.id_token);
   }
 
   // Set events
   userManager.events.addUserLoaded((user: User) => {
-    localStorage.setItem(SETTINGS.bearerTokenKey, user.id_token);
+    saveAccessToken(user.id_token);
   });
 
   userManager.events.addAccessTokenExpired(() => {

cc7c79a9ca3c700324ab2367bafdb55ce2c59046
Pavlo Getta <pavlo.getta@epfl.ch>
2019-08-23 15:57:57 +0200

    Remove redundant webpack config entries

--- apps/search-poc/src/web-components/resource-details/webpack.config.dev.js
+++ apps/search-poc/src/web-components/resource-details/webpack.config.dev.js
@@ -16,43 +16,6 @@ module.exports = {
     contentBase: path.resolve(__dirname, '../../../dist-wc/'),
     hot: true,
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
-  module: {
-      rules: [{
-        test:/\.css$/,
-        use: [{
-          loader: 'style-loader',
-          options: {
-            injectType: 'singletonStyleTag',
-            insert: function (styleElement) {
-              window.getResourceDetailsStyleEl = function() {
-                return styleElement;
-              }
-            },
-          },
-        },
-          'css-loader',
-        ]
-    }, {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              "@babel/preset-react",
-              "@babel/preset-typescript"
-            ],
-            plugins: [
-              ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-              "@babel/proposal-class-properties",
-              "@babel/proposal-object-rest-spread",
-            ]
-          }
-      }],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/web-components/resource-details/template.html',

eef7bb4a9d6efb113db05381b42f8a24c26ced92
kenjinp <github@kenny.wtf>
2019-10-07 15:59:36 +0200

    set morpho type to any for now

--- apps/search-poc/src/hooks/useMorphology.ts
+++ apps/search-poc/src/hooks/useMorphology.ts
@@ -28,14 +28,12 @@ export default (
     setLoading(true);
     setStage('Loading');
     nexus.File.get(org, proj, encodeURIComponent(id), { as: 'blob' })
-      // @ts-ignore
-      .then(morpho => {
+      .then((morpho: any) => {
         setStage('Converting');
         return morpho;
       })
       .then(convertToSwc)
-      // @ts-ignore
-      .then(morpho => {
+      .then((morpho: any) => {
         setStage('Parsing');
         return morpho;
       })

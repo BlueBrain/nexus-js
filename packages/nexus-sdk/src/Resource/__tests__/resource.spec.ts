0da6f60887305e65807559668f112704eb9567a4
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2019-11-07 13:34:53 +0100

    Added test for httpPut usecase

--- packages/nexus-sdk/src/Resource/__tests__/resource.spec.ts
+++ packages/nexus-sdk/src/Resource/__tests__/resource.spec.ts
@@ -171,7 +171,7 @@ describe('Resource', () => {
       expect(fetchMock.mock.calls[0][1].body).toEqual(JSON.stringify(payload));
       expect(fetchMock.mock.calls[0][1].method).toEqual('POST');
     });
-    it('should make httpPost call with schemaID  to the resources api, when schemaId is present', async () => {
+    it('should make httpPost call with schemaId  to the resources api, when schemaId is present', async () => {
       fetchMock.mockResponseOnce(JSON.stringify({ data: '' }));
       const payload = {
         '@id': 'myResource',
@@ -185,6 +185,44 @@ describe('Resource', () => {
       );
       expect(fetchMock.mock.calls[0][1].body).toEqual(JSON.stringify(payload));
       expect(fetchMock.mock.calls[0][1].method).toEqual('POST');
+    });
+
+    it('should make httpPut call  with ResourceId to the resources api, when ResourceId is present', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ data: '' }));
+      const payload = {
+        '@id': 'myResource',
+        '@context': 'something',
+        something: 'hello!',
+      };
+      await resource.create('org', 'project', payload, undefined, 'resourceId');
+      expect(fetchMock.mock.calls.length).toEqual(1);
+      expect(fetchMock.mock.calls[0][0]).toEqual(
+        'http://api.url/v1/resources/org/project/_/resourceId',
+      );
+      expect(fetchMock.mock.calls[0][1].body).toEqual(JSON.stringify(payload));
+      expect(fetchMock.mock.calls[0][1].method).toEqual('PUT');
+    });
+
+    it('should make httpPut call with schemaId and ResourceId to the resources api, when schemaId and ResourceId is present', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ data: '' }));
+      const payload = {
+        '@id': 'myResource',
+        '@context': 'something',
+        something: 'hello!',
+      };
+      await resource.create(
+        'org',
+        'project',
+        payload,
+        'schemaId',
+        'resourceId',
+      );
+      expect(fetchMock.mock.calls.length).toEqual(1);
+      expect(fetchMock.mock.calls[0][0]).toEqual(
+        'http://api.url/v1/resources/org/project/schemaId/resourceId',
+      );
+      expect(fetchMock.mock.calls[0][1].body).toEqual(JSON.stringify(payload));
+      expect(fetchMock.mock.calls[0][1].method).toEqual('PUT');
     });
   });
 

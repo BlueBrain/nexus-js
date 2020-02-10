d37e54e36ed17a814e394a095858ae8ec716743e
Stafeeva <n.stafeeva@gmail.com>
2020-02-10 16:40:42 +0100

    Update names

--- packages/nexus-sdk/src/Version/_tests_/version.spec.ts
+++ packages/nexus-sdk/src/Version/_tests_/version.spec.ts
@@ -3,13 +3,13 @@ import { mockFetchers } from '../../testUtils';
 
 const version = Version(mockFetchers, { uri: 'http://api.url' });
 
-describe('Views', () => {
+describe('Version', () => {
   beforeEach(() => {
     fetchMock.resetMocks();
   });
 
   describe('get', () => {
-    it('should make httpGet call to the schema api with the right url', async () => {
+    it('should make httpGet call to the version api with the right url', async () => {
       await version.get();
       expect(fetchMock.mock.calls.length).toEqual(1);
       expect(fetchMock.mock.calls[0][0]).toEqual('http://api.url/version');

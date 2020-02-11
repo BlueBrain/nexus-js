60772c722d4776ca38b445e9351bca988db10c39
Natalia Stafeeva <n.stafeeva@gmail.com>
2020-02-11 10:40:46 +0100

    Add Version get (#311)
    * Add version httpget
    
    * Add tests
    
    * Update names

---
+++ packages/nexus-sdk/src/Version/index.ts
@@ -0,0 +1,15 @@
+import { Fetchers } from '../types';
+import { Version } from './types';
+import { NexusContext } from '../nexusSdk';
+
+const Version = ({ httpGet }: Fetchers, context: NexusContext) => {
+  return {
+    get: (): Promise<Version> => {
+      return httpGet({
+        path: `${context.uri}/version`,
+      });
+    },
+  };
+};
+
+export default Version;

8fd5c59ad13207a1d87054b660d0b92a927729ab
Julien Machon <julienmachon@gmail.com>
2020-02-24 09:24:27 +0100

    add props types (#321)
    Co-authored-by: Kenneth Pirman <github@kenny.wtf>

--- packages/react-nexus/src/NexusProvider.tsx
+++ packages/react-nexus/src/NexusProvider.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import NexusContext from './nexusContext';
+import { NexusClient } from '@bbp/nexus-sdk';
 
-const NexusProvider = ({ nexusClient, children }: any) => {
+export type NexusProviderProps = {
+  nexusClient: NexusClient;
+};
+
+const NexusProvider: React.FC<NexusProviderProps> = ({
+  nexusClient,
+  children,
+}) => {
   return (
     <NexusContext.Provider value={nexusClient}>
       {children}

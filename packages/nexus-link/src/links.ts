1ae61fc11b60ea92d31068745c46a0f4fc4bc6bc
Dhanesh Neela Mana <dhanesh.neelamana@epfl.ch>
2021-02-11 09:21:54 +0100

    Remove un used code

--- packages/nexus-link/src/links.ts
+++ packages/nexus-link/src/links.ts
@@ -110,49 +110,6 @@ export const triggerFetch = (fetch?: any): Link => (
     : fetchObservable;
 };
 
-export const oldtriggerFetch = (fetch?: any): Link => (operation: Operation) =>
-  new Observable(observer => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    const { path, body, headers, method, context = {} } = operation;
-    const defaultHeaders = context.noDefaultHeader
-      ? {}
-      : {
-          'Content-Type': 'application/json',
-        };
-    fetch(path, {
-      body,
-      headers: { ...defaultHeaders, ...headers },
-      signal,
-      method,
-    })
-      .then(async (response: Response) => {
-        if (response.status >= 400) {
-          observer.error(await response.json());
-        } else {
-          const { parseAs } = context;
-          switch (parseAs) {
-            case FetchAs.TEXT:
-              observer.next(await response.text());
-            case FetchAs.BLOB:
-              observer.next(await response.blob());
-            case FetchAs.DOCUMENT:
-              observer.next(await response.formData());
-            case FetchAs.JSON:
-            default:
-              observer.next(await response.json());
-          }
-        }
-      })
-      .catch(error => {
-        observer.error(error);
-        observer.complete();
-      });
-
-    // On un-subscription, cancel the request
-    return () => controller.abort();
-  });
-
 export const poll = (pollIntervalMs: number): Link => (
   operation: Operation,
   forward?: Link,

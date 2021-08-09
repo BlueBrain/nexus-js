053e73d7088ccc072f4c017420b5c0e2ccb9b23c
Stafeeva <n.stafeeva@gmail.com>
2021-08-09 11:14:39 +0200

    Remove comments

--- packages/nexus-sdk/src/Quotas/index.ts
+++ packages/nexus-sdk/src/Quotas/index.ts
@@ -1,9 +1,3 @@
-// to support:
-// GET /v1/quotas/{org}/{proj}
-// ask back end for an example response
-// indexing - sync ?
-// include revision ?
-
 import { NexusContext } from '../nexusSdk';
 import { Fetchers } from '../types';
 import { Quota } from './types';
@@ -12,7 +6,7 @@ const Quotas = ({ httpGet }: Fetchers, context: NexusContext) => {
   return {
     get: (orgLabel: string, projectLabel: string): Promise<Quota> =>
       httpGet({
-        path: `${context.uri}/quotas/${orgLabel}/${projectLabel}`, // allow indexing - sync ?
+        path: `${context.uri}/quotas/${orgLabel}/${projectLabel}`,
       }),
   };
 };
